262a76fb29c7c9c80aaf32ada1469a3b482d16c5
Crystal <2358343663@qq.com>
2021-09-17 16:48:28 +0800

    最大子序和

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,15 +1,10 @@
-function insertSort(arr) {
-  const len = arr.length;
-  for (let i = 1; i < len; i++) {
-    let target = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (arr[target] < arr[j]) {
-        [arr[j], arr[target]] = [arr[target], arr[j]];
-        target--;
-      }
+function maxSum(nums, target) {
+  const hashMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (hashMap.has(target - nums[i])) {
+      return [hashMap.get(target - nums[i]), i];
+    } else {
+      hashMap.set(nums[i], i);
     }
   }
-  return arr;
 }
-
-console.log(insertSort([2, 3, 1, 4, 5, 1]));
