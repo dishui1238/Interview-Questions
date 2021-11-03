b68b8358f64273ce3af30cfd1f63b7464051754e
Crystal <2358343663@qq.com>
2021-11-03 09:51:37 +0800

    2021-11-03

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -108,3 +108,25 @@ function maxRequest(fn, maxNum) {
     help(maxNum);
   });
 }
+
+function debounce(fn, delay) {
+  let timeId;
+  return function (...args) {
+    clearTimeout(timeId);
+    timeId = setTimeout(() => {
+      fn.call(this, ...args);
+    }, delay);
+  };
+}
+
+function throttle(fn, delay) {
+  let canRun = true;
+  return function (...args) {
+    if (!canRun) return;
+    canRun = false;
+    setTimeout(() => {
+      fn.call(this, ...args);
+      canRun = true;
+    }, delay);
+  };
+}
